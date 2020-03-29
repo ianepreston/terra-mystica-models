ea48b5357dd21f8fee05be358545bfd8b827be5f
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-03-29 13:52:44 -0600

    score by turn model

--- terra_mystica_models/models/train_model.py
+++ terra_mystica_models/models/train_model.py
@@ -62,3 +62,32 @@ def interact_model(predict_df):
     X = sm.add_constant(predict_df[x_cols]).astype(int)
     lin_model = sm.OLS(y, X).fit()
     return lin_model
+
+
+def score_turn_model(predict_df):
+    """See how score and bonus tiles interact with player choice
+    
+    Now we add in which turn the score tile is active for
+    """
+    faction_cols = [
+        col
+        for col in predict_df.columns
+        if col.startswith("faction_") and "_x_" not in col
+    ]
+    bonus_regex = r"faction_\w+_x_BON\d+"
+    bonus_cols = [
+        col
+        for col in predict_df.columns
+        if re.match(bonus_regex, col) and not col.endswith("BON1")
+    ]
+    score_regex = r"faction_\w+_x_score_turn_\d_SCORE\d+"
+    score_cols = [
+        col
+        for col in predict_df.columns
+        if re.match(score_regex, col) and not col.endswith("SCORE1")
+    ]
+    x_cols = ["player_num"] + faction_cols + bonus_cols + score_cols
+    y = predict_df["vp_margin"]
+    X = sm.add_constant(predict_df[x_cols]).astype(int)
+    lin_model = sm.OLS(y, X).fit()
+    return lin_model
