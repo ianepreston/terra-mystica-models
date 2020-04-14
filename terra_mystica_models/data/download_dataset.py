ae43dd96799f10d8466772f432820f29eb829250
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-04-14 11:22:16 -0600

    add cutoff parameter

--- terra_mystica_models/data/download_dataset.py
+++ terra_mystica_models/data/download_dataset.py
@@ -1,4 +1,5 @@
 """Download all the JSON files containing information on Terra Mystica games"""
+import datetime as dt
 import json
 import re
 from pathlib import Path
@@ -7,7 +8,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def data_download():
+def _check_cutoff(json_string):
+    """Check if a file was available when this analysis was first run
+    
+    Parameters
+    ----------
+    json_string: str
+        Name of the json file to check
+    """
+    cutoff_date = dt.date(2020, 1, 1)
+    json_date = dt.datetime.strptime(
+        json_string.replace(".json", "-01"), "%Y-%m-%d"
+    ).date()
+    return json_date <= cutoff_date
+
+
+def data_download(cutoff_date=True):
+    """Download the JSON files from snelman
+    
+    Parameters
+    ----------
+    cutoff_date: bool, default True
+        Only download files up to the date that was available when this analysis was
+        originally done
+    """
     base_url = "https://terra.snellman.net/data/events/"
     html = requests.get(base_url).text
     soup = BeautifulSoup(html, "html.parser")
@@ -20,7 +44,11 @@ def data_download():
     for json_string in json_strings:
         json_url = base_url + json_string
         json_path = raw_folder / json_string
-        if not json_path.exists():
+        if cutoff_date:
+            download_condition = (not json_path.exists()) & _check_cutoff(json_string)
+        else:
+            download_condition = not json_path.exists()
+        if download_condition:
             with open(json_path, "w") as outfile:
                 json.dump(requests.get(json_url).json(), outfile)
     return True
